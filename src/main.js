f066a65c0f3347fdd98eab4eb079d44f6af8ee36
southboy12 <menkachi85@gmail.com>
2025-03-01 16:31:45 +0100

    added logic to place task in baed on status

--- src/main.js
+++ src/main.js
@@ -6,10 +6,6 @@ document.querySelectorAll(".openModal").forEach((item) => {
     });
 });
 
-// document.querySelectorAll(".openModal").addEventListener("click", function() {
-//     document.getElementById("modal").style.display = "flex";
-// });
-
 document.querySelector(".close").addEventListener("click", function() {
     document.getElementById("modal").style.display = "none";
 });
@@ -34,10 +30,10 @@ class Task {
 
 class TaskManager {
     constructor() {
-        this.tasks = [];
-        this.pendingTasks = document.querySelector('#card--pending');
-        this.progressTasks = document.querySelector('#card--progress');
-        this.completedTasks = document.querySelector('#card--complete');
+        this.tasks = []
+        this.pendingTasksContainer = document.querySelector('#card--pending');
+        this.progressTasksContainer = document.querySelector('#card--progress');
+        this.completedTasksContainer = document.querySelector('#card--complete');
         this.dashboardText = document.querySelector('.dashboard--text');
 
         this.addTask = document.querySelector('#submit--btn');
@@ -46,7 +42,7 @@ class TaskManager {
     }
 
     createTask(e) {
-        e.preventDefault(e);
+        e.preventDefault();
         const titleEl = document.querySelector('#title').value;
         const descriptionEl = document.querySelector('#description').value;
         const dueDateEl = document.querySelector('#dateInput').value;
@@ -58,10 +54,18 @@ class TaskManager {
         return new Task(titleEl, descriptionEl, dueDateEl, dueTimeEl, priorityEl, statusEl, noteEl);
     }
 
-    getCardHtml() {
-        return this.tasks.map((task, index) => {
-            return `<div class="card">
-            <h3 class="padding">${task.priority}</h3>
+    addCardToTask(e) {
+        const card = this.createTask(e);
+        if (!card) return;
+        this.tasks.push(card);
+        this.dashboardText.style.display = "none";
+        this.renderTasks();
+    }
+
+    getCardHtml(task) {
+        const priorityClass = `priority--${task.priority.toLowerCase()}`; 
+        return `<div class="card">
+            <h3 class="${priorityClass} padding">${task.priority}</h3>
             <br />
             <div class="padding task--date">
               <h4>Date Added</h4>
@@ -84,21 +88,20 @@ class TaskManager {
               <div class="task--note-delete"></div>
             </div>
           </div>`
-        }).join('');
-    }
-
-    addCardToTask(e) {
-        const card = this.createTask(e);
-        if (!card) return;
-        this.tasks.push(card);
-        this.dashboardText.style.display = "none";
-        this.renderTasks();
     }
 
     renderTasks() {
-        this.pendingTasks.innerHTML = this.getCardHtml();
-    } 
-}
+        this.pendingTasksContainer.innerHTML = "";
+        this.progressTasksContainer.innerHTML = "";
+        this.completedTasksContainer.innerHTML = "";
+        this.tasks.forEach((task) => {
+            if (task.status === "pending") {
+                this.pendingTasksContainer.innerHTML += this.getCardHtml(task);
+            } else {
+                this.progressTasksContainer.innerHTML += this.getCardHtml(task);
+            }
+    } )
+}}
 
 new TaskManager();
 
